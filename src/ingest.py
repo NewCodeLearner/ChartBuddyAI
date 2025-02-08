3ab61affe2db6a2b909b5dd121f6dd9b6149f39f
Girish Barhate <girish.barhate@gmail.com>
2025-02-08 13:19:30 +0530

    created ingest.py file add qdrant client

--- src/ingest.py
+++ src/ingest.py
@@ -1,1 +1,12 @@
 #Code file to import documents,images into Qdrant database
+from langchain_community.vectorstores import Qdrant
+from transformers import ViTImageProcessor, ViTModel
+from qdrant_client import QdrantClient
+import torch
+
+
+# Create Qdrant Client
+
+client = QdrantClient(host = 'localhost',port=6333)
+
+print(client)
