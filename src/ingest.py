086300a67124e12f8e80223c23f7c96c7e79b470
Girish Barhate <girish.barhate@gmail.com>
2025-02-09 20:51:55 +0530

    Create Base64 string representations to store metadata

--- src/ingest.py
+++ src/ingest.py
@@ -36,4 +36,32 @@ print(payloads)
 # 4. Create PIL (Python Imaging Library) image from each of the local URLs.
 # PIL provides a wide range of functions for image processing, such as resizing, cropping, rotating, and applying filters.
 images =  list(map(lambda imgurl : Image.open(imgurl),payloads['image_url']))
-print(images)
+#print(images)
+
+# 5. Create Base64 string representations to store alongside
+# metadata. This will allow us to preview the images.
+from io import BytesIO
+import math
+import base64
+
+target_width = 256
+
+def resize_image(image_url):
+    pil_image = Image.open(image_url)
+    image_aspect_ratio = pil_image.width / pil_image.height
+    resized_pil_image = pil_image.resize(
+        [target_width, math.floor(target_width * image_aspect_ratio)]
+    )
+    return resized_pil_image
+
+def convert_image_to_base64(pil_image):
+    image_data = BytesIO()
+    pil_image.save(image_data,format = "JPEG")
+    base64_string = base64.b64encode(image_data.getvalue()).decode("utf-8")
+    return base64_string
+
+resized_images = list(map(lambda img: resize_image(img),sample_image_urls))
+base64_strings = list(map(lambda el : convert_image_to_base64(el),resized_images))
+payloads['base64'] = base64_strings
+print(payloads)
+
