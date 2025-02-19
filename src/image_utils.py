121a7ac7b4bf3d40fab4a0b47ba8b1fe86fdc8d6
Girish Barhate <girish.barhate@gmail.com>
2025-02-19 12:49:59 +0530

    Search Qdrant for similar images

--- src/image_utils.py
+++ src/image_utils.py
@@ -3,6 +3,7 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 from PIL import Image
 import streamlit as st
 import io
+from qdrant_client import QdrantClient
 
 # Import the model and tokenizer then run 
 # all the images through it to create the embeddings.
@@ -10,7 +11,8 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 
 processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
 model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
-
+collection_name = "stock_charts_images"
+client = QdrantClient(host ='localhost',port=6333,prefix="qdrant",timeout=60)
 
 def set_selected_record(new_record):
     st.session_state.selected_record = new_record
@@ -37,5 +39,15 @@ def upload_and_display_image():
             with st.spinner("Searching for similar charts..."):
               # Ensure image vector is generated only once
                 st.session_state.uploaded_image_vector = get_image_vector(uploaded_image)
+
+            # Search Qdrant for similar images
+            search_results = client.search(
+                collection_name=collection_name,
+                query_vector=st.session_state.uploaded_image_vector,
+                limit=5
+            )
+
+            # Store results in session state to persist across reruns
+            st.session_state.uploaded_search_results = search_results
     
     return None
