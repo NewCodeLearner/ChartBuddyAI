311525b088fc3687a7a0b6222a3c2130c045c53a
Girish Barhate <girish.barhate@gmail.com>
2025-02-19 11:04:53 +0530

    modify image_utils code to handle image upload and display

--- src/image_utils.py
+++ src/image_utils.py
@@ -11,6 +11,10 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
 model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
 
+
+def set_selected_record(new_record):
+    st.session_state.selected_record = new_record
+
 # Converts an image to an embedding using image model
 def get_image_vector(image):
     inputs = processor(images=image,return_tensors="pt")
@@ -24,6 +28,13 @@ def upload_and_display_image():
     if uploaded_file:
         image = Image.open(uploaded_file).convert("RGB")
         st.image(image,caption="Uploaded Image",use_container_width=True)
+        st.button(
+
+            label = "Find similar charts",
+            key = id,
+            on_click = set_selected_record, # call set the selected fn 
+            args = [image]
+        ) 
         return image
     
     return None
