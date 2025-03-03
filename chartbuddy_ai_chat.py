a74d7c689f025b84aa655c9a0ee4590a3b5dc250
Girish Barhate <girish.barhate@gmail.com>
2025-03-03 18:24:36 +0530

    added Groq chat completion and related imports.

--- chartbuddy_ai_chat.py
+++ chartbuddy_ai_chat.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from groq import Groq
+import base64
 
 # Set up page configuration
 #st.set_page_config(page_title="ChartBuddy AI Chat", layout="wide")
@@ -40,14 +42,38 @@ if st.button("Send"):
         
         # Call your API here (Ollama, Groq, etc.) to generate a response.
         # For this example, we'll simulate a response.
-        simulated_response = f"Simulated response to: {user_message}"
+        #simulated_response = f"Simulated response to: {user_message}"
+
+        # Create Groq client
+        client = Groq()
+        
+        # Groq completion using Groq API Request: Call the chat.completions API endpoint.
+        chat_completion = client.chat.completions.create(
+            messages =[
+                {
+                    "role":"user",
+                    "content":[
+                        {"type":"text", "text" : "Whats in this image? which candlestick pattern do you recognize in this chart?"},
+                        {
+                            "type":"image_url",
+                            #we'll need to first encode our image to a base64 format string before passing it as the image_url in our API request
+                            "image_url":{
+                                "url" : base64.b64encode(st.session_state.selected_chart_image).decode('utf-8')
+                            }
+
+                        }
+                    ]
+                }
+            ],
+            model="llama-3.2-11b-vision-preview",
+        )
         
         # Append AI's response to chat history
         st.session_state.chat_history.append({
             "role": "assistant",
-            "content": simulated_response
+            "content": chat_completion.choices[0].message.content
         })
         
         # Clear the input box (optional) and refresh the page to show updated chat history.
-        st.experimental_rerun()
+        st.rerun()
 
